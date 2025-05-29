fdf4f3cad3f103210595738fd278c5c671edb85d
daumantaskodina <daumantas.banys@gmail.com>
2025-05-29 21:27:37 +0200

    Fix theme-provider import path in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type React from "react"
 import "./globals.css"
 import { Inter } from "next/font/google"
-import { ThemeProvider } from "@/components/theme-provider"
+import { ThemeProvider } from "../components/theme-provider"
 import { LanguageProvider } from "./language-context"
 
 const inter = Inter({ 
